986ac0c504bfb92848282e31f29731a37ed0981f
Tam Nguyen Phuoc <tamnp@nal.vn>
2021-09-28 15:25:49 +0700

    first commit

---
+++ src/main/java/com/tam/SpringbootDemoApplication.java
@@ -0,0 +1,21 @@
+package com.tam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDemoApplication {
+
+    @GetMapping("/message")
+    public String message() {
+        return "Success";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+
+}
